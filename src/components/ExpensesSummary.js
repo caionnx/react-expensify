408391f3bc44c70c8a8dd5f156bc3d7e74dd7a40
Caio Navarro <caionavarro.n@hotmail.com>
2018-04-22 10:30:51 -0300

    Fix mispelled in expenses summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -7,10 +7,11 @@ import selectExpenses from '../selectors/expenses';
 export const ExpensesSummary = (props) => {
     const visibleExpenses = selectExpenses(props.expenses, props.filters);
     const totalFromVisible = selectExpensesTotal(visibleExpenses);
+    const expenseWord = visibleExpenses.length === 1 ? 'expense' : 'expenses';
     
     return (
         <p>
-            Viewing {visibleExpenses.length} expenses totalling {numeral(totalFromVisible / 100).format('$0,0.00')}
+            Viewing {visibleExpenses.length} {expenseWord} totalling {numeral(totalFromVisible / 100).format('$0,0.00')}
         </p>
     )
 };
