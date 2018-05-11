50eb332d806bac2c6e109b732c213ec8a042e472
Caio Navarro <caionavarro.n@hotmail.com>
2018-05-10 23:05:37 -0300

    Add styles - part one

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
@@ -8,11 +9,17 @@ export const ExpensesSummary = (props) => {
     const visibleExpenses = selectExpenses(props.expenses, props.filters);
     const totalFromVisible = selectExpensesTotal(visibleExpenses);
     const expenseWord = visibleExpenses.length === 1 ? 'expense' : 'expenses';
+    const totalFromVisibleFormatted = numeral(totalFromVisible / 100).format('$0,0.00');
     
     return (
-        <p>
-            Viewing {visibleExpenses.length} {expenseWord} totalling {numeral(totalFromVisible / 100).format('$0,0.00')}
-        </p>
+      <div className="page-header">
+        <div className="content-container">
+          <h1 className="page-header__title">Viewing <span>{visibleExpenses.length}</span> {expenseWord} totalling <span>{totalFromVisibleFormatted}</span></h1>
+          <div className="page-header__actions">
+            <Link className="button" to="/create">Add Expense</Link>
+          </div>
+        </div>
+      </div>
     )
 };
 
