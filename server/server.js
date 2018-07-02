eb438f605f16225c62a614aa957556257d16b0dd
Caio Navarro <caionavarro.n@hotmail.com>
2018-07-01 22:37:38 -0300

    Add gzip compression for expressjs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const express = require('express')
+const compression = require('compression')
 const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
 
+app.use(compression())
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
