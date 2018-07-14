69e245f2212b0eaf9ad3215978c0ed2483feee88
Caio Navarro <caionavarro.n@hotmail.com>
2018-07-14 09:15:27 -0300

    Improve expenses category select by derived state from props and onChangeOption method

--- src/js/components/ExpensesCategorySelect.js
+++ src/js/components/ExpensesCategorySelect.js
@@ -3,18 +3,37 @@ import { connect } from 'react-redux'
 import { startSetCategories } from '../actions/categories'
 
 export class ExpensesCategorySelect extends React.Component {
-  componentDidMount () {
-    this.props.fillCategories()
+  state = {
+    value: null
   }
+
+  componentDidMount () {
+    if (this.props.categories && !this.props.categories.length) {
+      this.props.fillCategories()
+    }
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    if (!state.value) {
+      return { value: props.defaultValue }
+    }
+    return null
+  }
+
+  onChangeOption = (callbackFn) => (event) => {
+    this.setState({value: event.target.value})
+    return callbackFn && callbackFn(event.target.value)
+  }
+
   render () {
     const { categories, defaultValue } = this.props
     return (
       <select
         className='select'
-        value={defaultValue}
-        onChange={() => null}>
-        <option key={defaultValue}>{defaultValue}</option>
-        { categories.map(cat => <option key={cat.id}>{cat.value}</option>) }
+        value={this.state.value}
+        onChange={this.onChangeOption(this.props.onChange)}>
+        <option key={defaultValue} value='none'>{this.props.defaultText}</option>
+        { categories.map(ct => <option key={ct.id} value={ct.id}>{ct.value}</option>) }
       </select>
     )
   }
