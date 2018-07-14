12ca84086abf6a8ca251cb96d2bcbb00752ccb92
Caio Navarro <caionavarro.n@hotmail.com>
2018-07-14 09:13:40 -0300

    Add action to register categories when user has not yet configuration

--- src/js/actions/categories.js
+++ src/js/actions/categories.js
@@ -1,5 +1,9 @@
 import database from '../firebase/firebase'
 
+export const startRegisterCategories = (categories, uid) => {
+  return database.ref(`users/${uid}/categories`).set({...categories})
+}
+
 // SET_CATEGORIES
 export const setCategories = (categories) => ({
   type: 'SET_CATEGORIES',
@@ -29,6 +33,9 @@ export const startSetCategories = () => (dispatch, getState) => {
       database.ref(`categories`).once('value').then((defaultSnapshot) => {
         categories = transformToArray(defaultSnapshot)
         dispatch(setCategories(categories))
+
+        return defaultSnapshot.hasChildren() &&
+          startRegisterCategories(defaultSnapshot.val(), uid)
       })
     }
   })
