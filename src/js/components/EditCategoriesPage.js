ece5b63aa7c07f7b38833fd3791038f4cab36016
Caio Navarro <caionavarro.n@hotmail.com>
2018-07-16 22:47:51 -0300

    Add function do validate new category

--- src/js/components/EditCategoriesPage.js
+++ src/js/components/EditCategoriesPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Modal from 'react-modal'
+import slugify from 'slugify'
 import PropTypes from 'prop-types'
 import { startSetCategories, startRemoveCategory } from '../actions/categories'
 
 export class EditCategoriesPage extends React.Component {
   state = {
     removeCategoryModal: false,
-    idToRemove: false
+    idToRemove: false,
+    error: ''
   }
   onOpenRemoveCategoryModal = (id) => {
     this.setState({ removeCategoryModal: true, idToRemove: id })
@@ -20,6 +22,21 @@ export class EditCategoriesPage extends React.Component {
       this.props.startRemoveCategory({ id: this.state.idToRemove }).then(() => {
         this.setState({ removeCategoryModal: false })
       })
+    }
+  }
+  onAddCategory = (event) => {
+    event.preventDefault()
+    const node = event.target.querySelector(`#new-category`)
+    const value = node && node.value
+    const slug = slugify(value, { replacement: '-', remove: null, lower: true })
+
+    if (slug === '') {
+      this.setState(() => ({ error: 'Please provide category name.' }))
+    } else if (this.props.categories.filter((item) => item.id === slug).length) {
+      this.setState(() => ({ error: 'Category already registered.' }))
+    } else {
+      this.setState(() => ({ error: '' }))
+      node.value = ''
     }
   }
   componentDidMount () {
@@ -74,6 +91,18 @@ export class EditCategoriesPage extends React.Component {
               <button id='closeRemoveExpenseModal' className='button' onClick={this.onCloseRemoveCategoryModal}>Cancel</button>
             </div>
           </Modal>
+
+          {this.state.error && <p className='form__error'>{this.state.error}</p>}
+          <form onSubmit={this.onAddCategory}>
+            <div className='input-group'>
+              <div className='input-group__item'>
+                <input id='new-category' className='text-input' placeholder='New Category' type='text' />
+              </div>
+              <div className='input-group__item'>
+                <button className='button'>Add Category</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     )
