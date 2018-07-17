7c285373ded3bb0f0ac3792968f8d386bf54dc94
Caio Navarro <caionavarro.n@hotmail.com>
2018-07-16 22:01:01 -0300

    Add reducer case to remove category from state

--- src/js/reducers/categories.js
+++ src/js/reducers/categories.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'SET_CATEGORIES':
       return action.categories
+    case 'REMOVE_CATEGORY':
+      return state.filter(({ id }) => id !== action.id)
     default:
       return state
   }
