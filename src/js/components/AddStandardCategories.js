a85fd3fa3314f72bba57a0b1df9b62c004c0be4f
Caio Navarro <caionavarro.n@hotmail.com>
2018-09-09 16:10:41 -0300

    Add jest test for AddStandardCategories

--- src/js/components/AddStandardCategories.js
+++ src/js/components/AddStandardCategories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const AddStandardCategories = ({ buttonAction }) => (
+export const AddStandardCategories = ({ buttonAction }) => (
   <div className='list-item list-item--message'>
     <div>
       <h3 className='list-item__title'>You dont have any category registered.</h3>
