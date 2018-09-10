95962c56a687e495b8d1d41aa4c4e0cdb0af09e4
Caio Nunes <caionavarro.n@hotmail.com>
2018-09-10 18:35:41 -0300

    Add style to remove category button

--- src/js/components/EditCategoryItem.js
+++ src/js/components/EditCategoryItem.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 export const EditCategoryItem = ({ category, buttonAction }) => (
   <div className='list-item' key={category.id}>
     <h3 className='list-item__title'>{category.value}</h3>
-    <button onClick={() => buttonAction(category.id)}>
-      <svg viewBox='0 0 12 12' width='12' height='12'>
+    <button title='Remove category' className='button button--link button--hover-close' onClick={() => buttonAction(category.id)}>
+      <svg className='icon' viewBox='0 0 12 12' width='12' height='12'>
         <path fillRule='evenodd' d='M11.53.47a.75.75 0 0 0-1.061 0l-4.47 4.47L1.529.47A.75.75 0 1 0 .468 1.531l4.47 4.47-4.47 4.47a.75.75 0 1 0 1.061 1.061l4.47-4.47 4.47 4.47a.75.75 0 1 0 1.061-1.061l-4.47-4.47 4.47-4.47a.75.75 0 0 0 0-1.061z' />
       </svg>
     </button>
