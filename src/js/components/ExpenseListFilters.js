5fd286b4c765af85564b38f04a850771cb447eee
Caio Nunes <caionavarro.n@hotmail.com>
2018-09-17 15:30:56 -0300

    Add button to clear filter of dates on ExpenseListFilters

--- src/js/components/ExpenseListFilters.js
+++ src/js/components/ExpenseListFilters.js
@@ -16,6 +16,10 @@ export class ExpenseListFilters extends React.Component {
   state = {
     calendarFocused: null
   }
+  onClearDates = () => {
+    this.props.setStartDate('')
+    this.props.setEndDate('')
+  }
   onStartDateChange = (startDate) => {
     this.props.setStartDate(startDate)
   }
@@ -106,6 +110,11 @@ export class ExpenseListFilters extends React.Component {
                 onDayChange={this.onEndDateChange}
               />
             </span>
+            <button onClick={this.onClearDates} type='button' aria-label='Clear Dates' className=''>
+              <svg className="icon" viewBox="0 0 12 12" width="12" height="12">
+              <path fillRule="evenodd" d="M11.53.47a.75.75 0 0 0-1.061 0l-4.47 4.47L1.529.47A.75.75 0 1 0 .468 1.531l4.47 4.47-4.47 4.47a.75.75 0 1 0 1.061 1.061l4.47-4.47 4.47 4.47a.75.75 0 1 0 1.061-1.061l-4.47-4.47 4.47-4.47a.75.75 0 0 0 0-1.061z"></path>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
