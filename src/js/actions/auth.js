69d7a6a8d2ea82cbc28cbc942af05b7e0161f9bc
Caio Navarro <caionavarro.n@hotmail.com>
2018-10-01 23:56:14 -0300

    Add action of create user on auth actions

--- src/js/actions/auth.js
+++ src/js/actions/auth.js
@@ -1,9 +1,13 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase'
+import { firebase, googleAuthProvider, createUser } from '../firebase/firebase'
 
 export const login = (uid) => ({
   type: 'LOGIN',
   uid
 })
+
+export const startCreateUser = (email, password) => () => {
+  return createUser(email, password)
+}
 
 export const startGoogleLogin = () => () => {
   return firebase.auth().signInWithPopup(googleAuthProvider)
