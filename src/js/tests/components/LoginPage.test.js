63df355720251768fad1ee546519960786d4dc76
Caio Navarro <caionavarro.n@hotmail.com>
2018-10-03 23:04:05 -0300

    Fix LoginPage jest test after new method of login

--- src/js/tests/components/LoginPage.test.js
+++ src/js/tests/components/LoginPage.test.js
@@ -15,7 +15,41 @@ test('should render LoginPage correctly', () => {
 test('should call startGoogleLogin on button click', () => {
   const startGoogleLogin = jest.fn()
   const wrapper = shallow(<LoginPage startGoogleLogin={startGoogleLogin} />)
-  wrapper.find('button').simulate('click')
+  wrapper.find('button').at(0).simulate('click')
 
   expect(startGoogleLogin).toHaveBeenCalled()
+})
+
+test('should show LoginEmailForm on button click', () => {
+  const wrapper = shallow(<LoginPage startGoogleLogin={startLoginProp} />)
+  wrapper.find('button').at(1).simulate('click', { preventDefault: jest.fn() })
+  const LoginEmailForm = wrapper.find('Connect(LoginEmailForm)')
+
+  expect(LoginEmailForm).toHaveLength(1)
+})
+
+test('should show CreateUserForm on button click', () => {
+  const wrapper = shallow(<LoginPage startGoogleLogin={startLoginProp} />)
+  wrapper.find('a').simulate('click', { preventDefault: jest.fn() })
+  const CreateUserForm = wrapper.find('Connect(CreateUserForm)')
+
+  expect(CreateUserForm).toHaveLength(1)
+})
+
+test('should pass state modifier to CreateUserForm component', () => {
+  const wrapper = shallow(<LoginPage startGoogleLogin={startLoginProp} />)
+  const spy = jest.spyOn(wrapper.instance(), 'toggleStateParam')
+  wrapper.setState({ logInWithEmail: true, createUser: true })
+  wrapper.find('Connect(CreateUserForm)').prop('goBackFunction')({ preventDefault: jest.fn() })
+
+  expect(spy).toHaveBeenCalled()
+})
+
+test('should pass state modifier to LoginEmailForm component', () => {
+  const wrapper = shallow(<LoginPage startGoogleLogin={startLoginProp} />)
+  const spy = jest.spyOn(wrapper.instance(), 'toggleStateParam')
+  wrapper.setState({ logInWithEmail: true, createUser: true })
+  wrapper.find('Connect(LoginEmailForm)').prop('goBackFunction')({ preventDefault: jest.fn() })
+
+  expect(spy).toHaveBeenCalled()
 })
