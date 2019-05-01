8986e8689ed9c9d8f346e7bfd63c9d7958995d08
Caio Navarro <caionavarro.n@hotmail.com>
2019-05-01 15:37:30 +1200

    Update express server to requests as a PWA

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
 
+app.get('/offline.html', (req, res) => {
+  res.sendFile(path.join(publicPath, 'offline.html'))
+})
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.join(publicPath, 'service-worker.js'))
+})
+
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz'
   res.set('Content-Encoding', 'gzip')
