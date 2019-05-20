9f129d38e4bb226276fd88cc1f62ec253b20030b
Caio Navarro <caionavarro.n@hotmail.com>
2019-05-20 19:52:15 +1200

    Change server to handle new path of files and better file organization

--- server/server.js
+++ server/server.js
@@ -4,33 +4,35 @@ const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
 
-app.get('/offline.html', (req, res) => {
-  res.sendFile(path.join(publicPath, 'offline.html'))
-})
+// Just a signal for development
+app.listen(port, () => console.log('Server is up!'))
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.join(publicPath, 'service-worker.js'))
-})
-
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz'
-  res.set('Content-Encoding', 'gzip')
+// Handle css and js extension (Because of Brotli compression)
+function handleVendorsCompress (req, res, next) {
+  req.url = req.url + '.br'
+  res.set('Content-Encoding', 'br')
+  if (req.url.match(/.css/g)) res.set('Content-Type', 'text/css; charset=UTF-8')
   next()
-})
+}
+app.get('*.css', handleVendorsCompress)
+app.get('*.js', handleVendorsCompress)
 
-app.get('*.css', function (req, res, next) {
-  req.url = req.url + '.gz'
-  res.set('Content-Encoding', 'gzip')
-  res.set('Content-Type', 'text/css; charset=UTF-8')
-  next()
-})
+// Handle all routing and root middleware
+app.use(express.static(path.join(publicPath, 'dist/')))
+app.get('*', ({ url }, res) => {
+  let fromPublic = false
 
-app.use(express.static(publicPath))
+  if (
+    url.match(/\/images\//) ||
+    url.match(/\/manifest.json/) ||
+    url.match(/\/install.js/) ||
+    url.match(/\/service-worker.js/) ||
+    url.match(/\/offline.html/)
+  ) fromPublic = true
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'))
-})
-
-app.listen(port, () => {
-  console.log('Server is up!')
+  if (fromPublic) {
+    res.sendFile(path.join(publicPath, url))
+  } else {
+    res.sendFile(path.join(publicPath, 'dist', 'index.html'))
+  }
 })
