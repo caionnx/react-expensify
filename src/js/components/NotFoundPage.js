bf336d6a25d1ab5f52dd5cf4c942d7fc1a45ce9b
Caio Navarro <caionavarro.n@hotmail.com>
2019-05-31 14:06:06 +1200

    Improve not found page interface

--- src/js/components/NotFoundPage.js
+++ src/js/components/NotFoundPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => (
-  <div>
-    404 - <Link to='/'>Go home</Link>
+  <div className='page-header'>
+    <div className='content-container'>
+      <h1 className='page-header__title'>Woops, 404.</h1>
+      <div className='page-header__actions'>
+        <a className='button' href='/'>Go Home</a>
+      </div>
+    </div>
   </div>
 )
 
